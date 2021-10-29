76ad64529f3b08f8e6f0e0d0b4f3e334dedf15c9
AttractiveMinki <70371239+AttractiveMinki@users.noreply.github.com>
2021-10-29 10:56:05 +0900

    fix: delete codes

--- frontend/src/store/modules/mypage.js
+++ frontend/src/store/modules/mypage.js
@@ -7,13 +7,6 @@ export default {
     transition: null,
   }),
   mutations: {},
-  actions: {
-    // 웹소켓으로 메시지 발신 action
-    // sendMessage(context, message) {
-    //   var jsonMessage = JSON.stringify(message);
-    //   console.log('Sending message: ' + jsonMessage);
-    //   context.state.ws.send(jsonMessage);
-    // },
-  },
+  actions: {},
   getters: {},
 };
