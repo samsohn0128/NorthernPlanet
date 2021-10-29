432c4629fa9e598c691b960a42cf9728dfd6df91
AttractiveMinki <70371239+AttractiveMinki@users.noreply.github.com>
2021-10-29 10:52:37 +0900

    fix: delete websocket code

--- frontend/src/store/modules/mypage.js
+++ frontend/src/store/modules/mypage.js
@@ -9,11 +9,11 @@ export default {
   mutations: {},
   actions: {
     // 웹소켓으로 메시지 발신 action
-    sendMessage(context, message) {
-      var jsonMessage = JSON.stringify(message);
-      /* console.log('Sending message: ' + jsonMessage); */
-      context.state.ws.send(jsonMessage);
-    },
+    // sendMessage(context, message) {
+    //   var jsonMessage = JSON.stringify(message);
+    //   console.log('Sending message: ' + jsonMessage);
+    //   context.state.ws.send(jsonMessage);
+    // },
   },
   getters: {},
 };
