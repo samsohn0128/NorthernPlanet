ac5034a22e9480872fe1337b114d580443a6872b
AttractiveMinki <70371239+AttractiveMinki@users.noreply.github.com>
2021-11-01 17:24:39 +0900

    feat: add CurrentPPTInfo in vuex

--- frontend/src/store/modules/mypage.js
+++ frontend/src/store/modules/mypage.js
@@ -1,12 +1,44 @@
 export default {
   namespaced: true,
   state: () => ({
-    currentPage: null,
-    location: null,
+    currentPPTID: null,
+    currentPPTTitle: null,
     size: null,
     transition: null,
   }),
-  mutations: {},
-  actions: {},
-  getters: {},
+  mutations: {
+    GET_CURRENT_PRESENTATION_INFO: function (state, data) {
+      state.currentPPTID = data.presentationId;
+      state.currentPPTTitle = data.presentationName;
+      console.log(state, 'get');
+    },
+    SET_CURRENT_PRESENTATION_INFO: function (state, data) {
+      data.presentationId = state.currentPPTID;
+      data.presentationName = state.currentPPTTitle;
+      console.log(data, 'set');
+    },
+    // INIT_CURRENT_PRESENTATION_INFO: function (state) {
+    //   state.currentPPTID = null;
+    //   state.currentPPTTitle = null;
+    // },
+  },
+  actions: {
+    getCurrentPresenatationInfo: function ({ commit }, data) {
+      commit('GET_CURRENT_PRESENTATION_INFO', data);
+    },
+    setCurrentPresenatationInfo: function ({ commit }, data) {
+      commit('SET_CURRENT_PRESENTATION_INFO', data);
+    },
+    // initPresentationInfo: function ({ commit }) {
+    //   commit('INIT_CURRENT_PRESENTATION_INFO');
+    // },
+  },
+  getters: {
+    getCurrentId(state) {
+      return state.currentPPTID;
+    },
+    getCurrentTitle(state) {
+      return state.currentPPTTitle;
+    },
+  },
 };
