52cef55cbfa7d8401611c57e91e1056c90df59c4
DongWoo Sohn <samsohn0128@gmail.com>
2021-11-03 14:38:40 +0900

    feat: [BE] pdf2png
    pdf file can be uploaded

---
+++ backend/northernplanet/src/main/java/com/pyhu/northernplanet/api/request/PptPdf2PngReq.java
@@ -0,0 +1,17 @@
+package com.pyhu.northernplanet.api.request;
+
+import io.swagger.annotations.ApiParam;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+
+public class PptPdf2PngReq {
+
+  @ApiParam(value = "사용자 ID", required = true)
+  private Long userId;
+
+  @ApiParam(value = "발표 자료", required = true)
+  private MultipartFile pptPdf;
+
+}
