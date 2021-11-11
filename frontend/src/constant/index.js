8189d1d15fd4222496dd918e089b1e5a2a84c09a
yesfordev <yesfordev@gmail.com>
2021-11-11 16:35:36 +0900

    chore: set deploy setting

--- frontend/src/constant/index.js
+++ frontend/src/constant/index.js
@@ -4,5 +4,5 @@ const BASE_URL = 'https://localhost:8446/api';
 //   'https://localhost:8446/oauth2/authorize/google?redirect_uri=https://localhost:3000/oauth2/redirect';
 //8443포트
 const GOOGLE_OAUTH_URL =
-  'https://localhost:8446/oauth2/authorize/google?redirect_uri=https://localhost:8443/oauth2/redirect';
+  'https://k5a204.p.ssafy.io:8446/oauth2/authorize/google?redirect_uri=https://localhost:8443/oauth2/redirect';
 export { BASE_URL, GOOGLE_OAUTH_URL };
