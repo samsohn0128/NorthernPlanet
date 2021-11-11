a1b8ff85a7acf225ae489e463a97bb7fc3d6ee7e
Minji Kim <mjkim103301@gmail.com>
2021-11-12 02:13:21 +0900

    feat: [FE] show script in meeting room

--- frontend/src/store/modules/meetingRoom.js
+++ frontend/src/store/modules/meetingRoom.js
@@ -32,6 +32,7 @@ export default {
     presentationContents: null, // 최초 API 요청으로 콘텐츠들의 size, user_id 받아옴
     /* imageUrls: null, */
     imageSrcs: null,
+    scriptList: null,
     selectedContentId: null,
     transition: null,
     // 초기 카메라, 마이크 세팅
@@ -127,6 +128,9 @@ export default {
       state.size = state.size === null ? '2' : state.size;
       state.transition =
         state.transition === null ? 'default' : state.transition;
+    },
+    SET_SCRIPTS(state, scriptList) {
+      state.scriptList = scriptList;
     },
     SET_ONGOING_PRESENTATION(state, { message, imageSrcs }) {
       state.imageSrcs = imageSrcs;
@@ -379,12 +383,15 @@ export default {
         url: `${API_SERVER_URL}/presentation/${context.state.myId}/${message.presentationUserId}`,
       }).then(res => {
         const imageSrcs = [];
+        const scriptList = [];
         console.log('slidelist: ', res.data.slideList);
         res.data.slideList.forEach(data => {
           let imageSrc = 'data:image/jpeg;base64,' + data.slideFile;
           imageSrcs.push(imageSrc);
+          scriptList.push(data.script);
         });
         context.commit('SET_IMAGE_SRCS', imageSrcs);
+        context.commit('SET_SCRIPTS', scriptList);
       });
       context.commit('CHANGE_CONTENT', message);
     },
@@ -399,11 +406,12 @@ export default {
       if (message.presentationUserId !== null) {
         axios({
           method: 'get',
-          url: `${API_SERVER_URL}/board/image/${context.state.roomNumber}/${message.presentationUserId}`,
+          url: `${API_SERVER_URL}/presentation/${context.state.myId}/${message.presentationUserId}`,
         }).then(res => {
           const imageSrcs = [];
-          res.data.forEach(imageStr => {
-            let imageSrc = 'data:image/jpeg;base64,' + imageStr;
+
+          res.data.forEach(data => {
+            let imageSrc = 'data:image/jpeg;base64,' + data.slideFile;
             imageSrcs.push(imageSrc);
           });
           context.commit('SET_ONGOING_PRESENTATION', { message, imageSrcs });
