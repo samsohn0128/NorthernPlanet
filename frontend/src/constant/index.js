c2cacada6cdbe206bc2e0de84172e5bc99172074
yesfordev <yesfordev@gmail.com>
2021-11-12 09:46:08 +0900

    chore: setting for deploy

--- frontend/src/constant/index.js
+++ frontend/src/constant/index.js
@@ -1,8 +1,8 @@
-const BASE_URL = 'https://k5a204.p.ssafy.io/api';
+const BASE_URL = 'https://k5a204.p.ssafy.io:8446/api';
 //프론트 3000 - 백엔드 8446포트에서 테스트 할 때 사용하세요.
 // const GOOGLE_OAUTH_URL =
 //   'https://localhost:8446/oauth2/authorize/google?redirect_uri=https://localhost:3000/oauth2/redirect';
 //8443포트
 const GOOGLE_OAUTH_URL =
-  'https://k5a204.p.ssafy.io/oauth2/authorize/google?redirect_uri=https://k5a204.p.ssafy.io:8443/oauth2/redirect';
+  'https://k5a204.p.ssafy.io:8446/oauth2/authorize/google?redirect_uri=https://k5a204.p.ssafy.io:8443/oauth2/redirect';
 export { BASE_URL, GOOGLE_OAUTH_URL };
