31c948a9a8fd924ed0c3c1b201a71f65de08f6f8
Minji Kim <mjkim103301@gmail.com>
2021-11-15 15:17:01 +0900

    feat: [FE] toastwarning 으로 변경

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,28 +2,31 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
 import store from '@/store';
-
+import Toast from 'vue-toastification';
+Vue.use(Toast);
 Vue.use(VueRouter);
 const router = new VueRouter({
   //8443포트에서 테스트할때는 history 모드 주석처리하세요.
-  //mode: 'history',
+  mode: 'history',
   routes,
 });
 
 router.beforeEach(function (to, from, next) {
   var authRequired = to.matched.some(routeInfo => {
-    // console.log(routeInfo);
     return routeInfo.meta.authRequired;
   });
   if (!authRequired || (authRequired && store.getters['users/getToken'])) {
-    // console.log('authRequired : ' + authRequired);
-    // console.log('isLogin : ' + store.state.users.login.isLogin);
     next();
   } else {
-    this.$toastInfo('로그인이 필요합니다');
+    Vue.$toast.warning('로그인이 필요합니다', {
+      timeout: 2000,
+      draggable: false,
+      position: 'bottom-right',
+      pauseOnFocusLoss: false,
+      pauseOnHover: false,
+    });
+
     router.push('/login');
-    // console.log('authRequired : ' + authRequired);
-    // console.log('isLogin : ' + store.state.users.login.isLogin);
   }
 });
 
