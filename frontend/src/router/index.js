cf002f779dfd8c74671a71c2173c12044b0115c8
Minji Kim <mjkim103301@gmail.com>
2021-11-15 15:47:18 +0900

    chore: google login link in 3000 port

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,8 +2,8 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
 import store from '@/store';
-import Toast from 'vue-toastification';
-Vue.use(Toast);
+//import Toast from 'vue-toastification';
+//Vue.use(Toast);
 Vue.use(VueRouter);
 const router = new VueRouter({
   //8443포트에서 테스트할때는 history 모드 주석처리하세요.
@@ -18,13 +18,13 @@ router.beforeEach(function (to, from, next) {
   if (!authRequired || (authRequired && store.getters['users/getToken'])) {
     next();
   } else {
-    Vue.$toast.warning('로그인이 필요합니다', {
-      timeout: 2000,
-      draggable: false,
-      position: 'bottom-right',
-      pauseOnFocusLoss: false,
-      pauseOnHover: false,
-    });
+    // Vue.$toast.warning('로그인이 필요합니다', {
+    //   timeout: 2000,
+    //   draggable: false,
+    //   position: 'bottom-right',
+    //   pauseOnFocusLoss: false,
+    //   pauseOnHover: false,
+    // });
 
     router.push('/login');
   }
