822760e6bb676c31b6dca47321e1502fb650f8cb
AttractiveMinki <70371239+AttractiveMinki@users.noreply.github.com>
2021-11-17 13:40:13 +0900

    fix: [FE] remove Slider

--- frontend/src/views/meetingRoom/components/PresentationLocationSelector.vue
+++ frontend/src/views/meetingRoom/components/PresentationLocationSelector.vue
@@ -1,53 +1,5 @@
 <template>
   <div class="d-flex flex-column justify-content-center align-items-center">
-    <div class="small-img-container">
-      <!-- prev -->
-      <PresentationSlideItem
-        v-if="prev >= 0"
-        :slideUrl="slideUrls[prev]"
-        :Next="false"
-        @selectPrev="progressPrev"
-        @selectNext="progressNext"
-      />
-      <img
-        v-else
-        src="@/assets/presentationTemplates/first-slide.png"
-        alt=""
-        class="small-img-setting"
-      />
-      <!-- next -->
-      <PresentationSlideItem
-        v-if="next < slideUrls.length"
-        :slideUrl="slideUrls[next]"
-        :Next="true"
-        @selectPrev="progressPrev"
-        @selectNext="progressNext"
-      />
-      <img
-        v-else
-        src="@/assets/presentationTemplates/last-slide.png"
-        alt=""
-        class="small-img-setting"
-      />
-    </div>
-    <!-- slider: 슬라이드 넘기기 ui -->
-    <div class="d-flex justify-content-center align-items-center mb-5">
-      <button class="slider-prev-button" @click="progressPrev">prev</button>
-      <button @keyup.right="progressNext" @keyup.left="progressPrev">
-        <div class="slider-progress-indicator">
-          {{ now + 1 }}/{{ slideUrls.length }}
-        </div>
-      </button>
-      <button class="slider-next-button" @click="progressNext">next</button>
-    </div>
-    <!-- slider: 슬라이드 넘기기 ui -->
-    <!-- slider alert: 첫 슬라이드, 마지막 슬라이드 alert -->
-    <transition name="fade">
-      <div v-if="alertShow" class="access-alert">
-        <h5 class="mb-0">{{ alertMessage }}</h5>
-      </div>
-    </transition>
-    <!-- slider alert: 첫 슬라이드, 마지막 슬라이드 alert -->
     <div class="button-setting">
       <!-- location -->
       <button
@@ -126,21 +78,15 @@
 
 <script>
 // import "./template.scss";
-import PresentationSlideItem from './PresentationSlideItem.vue';
 
 export default {
   name: 'PresentationLocationSelector',
-  components: { PresentationSlideItem },
+  components: {},
   // : props
   props: {},
   // : data
   data() {
     return {
-      prev: null,
-      now: null,
-      next: null,
-      alertMessage: null,
-      alertShow: false,
       selectedSize: null,
       selectedLocation: null,
     };
@@ -196,45 +142,18 @@ export default {
       };
       this.$store.dispatch('meetingRoom/sendMessage', message);
     },
+    // '$store.state.plus': function () {
+    //   console.log('InPresentationLocation', this.$store.state.plus);
+    // },
   },
   // : lifecycle hook
   mounted() {
-    this.prev = this.currentPage - 1;
-    this.now = this.currentPage;
-    this.next = this.currentPage + 1;
     // 매번 들어 올때, location, size를 state에 설정된 값으로 설정
     this.selectedSize = this.presentationSize;
     this.selectedLocation = this.presentationLocation;
   },
   // : methods
   methods: {
-    progressPrev: function () {
-      if (this.now > 0) {
-        this.prev -= 1;
-        this.now -= 1;
-        this.next -= 1;
-      } else {
-        this.alertMessage = '첫 번째 슬라이드입니다.';
-        this.activeAlert();
-      }
-    },
-    progressNext: function () {
-      if (this.now < this.slideUrls.length - 1) {
-        this.prev += 1;
-        this.now += 1;
-        this.next += 1;
-      } else {
-        this.alertMessage = '마지막 슬라이드입니다.';
-        this.activeAlert();
-      }
-    },
-    activeAlert: function () {
-      this.alertShow = true;
-      setTimeout(this.inactivateAlert, 2000);
-    },
-    inactivateAlert: function () {
-      this.alertShow = false;
-    },
     selectSize: function (size) {
       this.selectedSize = size;
     },
@@ -276,42 +195,6 @@ export default {
 </script>
 
 <style scoped>
-.progress-button {
-  cursor: pointer;
-}
-
-.slider-prev-button {
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border: none;
-  width: 125px;
-  height: 35px;
-  border-radius: 30px 0px 0px 30px;
-  color: white;
-  font-weight: bold;
-  background: linear-gradient(90deg, #2c3153 0%, #15182a 100%);
-}
-.slider-next-button {
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border: none;
-  width: 125px;
-  height: 35px;
-  border-radius: 0px 30px 30px 0px;
-  color: white;
-  font-weight: bold;
-  background: linear-gradient(90deg, #2c3153 0%, #15182a 100%);
-}
-.slider-progress-indicator {
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border: none;
-  width: 100px;
-  height: 35px;
-  color: white;
-  font-weight: bold;
-  background: linear-gradient(90deg, #a0b0d0 0%, #7587a6 100%);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 .button-setting {
   display: flex;
   flex-direction: column;
@@ -320,59 +203,6 @@ export default {
 .border-setting {
   border: 0px;
 }
-.overlay {
-  position: absolute;
-  bottom: 0;
-  border-radius: 25px;
-  background: rgba(0, 0, 0, 0.7);
-  width: 100%;
-  height: 100%;
-  opacity: 1;
-  color: white;
-  font-size: 2rem;
-  line-height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.access-alert {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  width: 75%;
-  border-radius: 10px;
-  background: rgba(255, 255, 255, 0.774);
-  color: black;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transform: translate(-50%, -50%);
-  transition: 0.5s;
-}
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-.small-img-container {
-  margin-top: 10px;
-  margin-bottom: 10px;
-  width: 350px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-}
-.small-img-setting {
-  max-width: 190px;
-  max-height: 100px;
-  padding: 5px;
-  cursor: pointer;
-}
-
 .template-container {
   position: relative;
   width: 240px;
