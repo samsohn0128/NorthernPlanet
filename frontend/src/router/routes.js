6d970c9bcfe7c801d3d3c700443cf4a5f5dee61d
AttractiveMinki <70371239+AttractiveMinki@users.noreply.github.com>
2021-11-18 04:01:06 +0900

    merge

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -82,7 +82,7 @@ export default [
     path: '/meetingroom/:roomNumber',
     name: 'MeetingRoom',
     component: MeetingRoom,
-    meta: { authRequired: true },
+    meta: { authRequired: false },
   },
   {
     path: '/mypage',
