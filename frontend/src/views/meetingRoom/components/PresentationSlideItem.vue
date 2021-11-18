3de5ed8867ca1cb43cdf6ced7ab21d4fc96b41d9
AttractiveMinki <70371239+AttractiveMinki@users.noreply.github.com>
2021-11-18 18:04:12 +0900

    feat: [FE] fix Slider pictures row to column

--- frontend/src/views/meetingRoom/components/PresentationSlideItem.vue
+++ frontend/src/views/meetingRoom/components/PresentationSlideItem.vue
@@ -50,15 +50,15 @@ export default {
 <style scoped>
 .slide-container {
   position: relative;
-  width: 150px;
-  height: 150px;
+  width: 100%;
+  height: 200px;
   margin-bottom: 20px;
 }
 .slide-insert {
   border-radius: 25px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  width: 150px;
-  height: 150px;
+  width: 100%;
+  height: 100%;
   cursor: pointer;
 }
 </style>
