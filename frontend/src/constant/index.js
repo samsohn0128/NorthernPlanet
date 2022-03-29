1b61df55f1b1bbd53edd852a9e34cbe531abf32d
Dongwoo Sohn <ssafy_coach_67@ssafy.com>
2022-03-29 00:48:23 +0000

    update Google_Oauth_Url

--- frontend/src/constant/index.js
+++ frontend/src/constant/index.js
@@ -1,6 +1,7 @@
 // const BASE_URL = 'https://localhost:8446/api';
 // 배포 설정
-const BASE_URL = 'https://k5a204.p.ssafy.io/api';
+// const BASE_URL = 'https://k5a204.p.ssafy.io/api';
+const BASE_URL = 'https://northernplanet.p.ssafy.io/api';
 const FILE_PATH = BASE_URL + '/presentation';
 //프론트 3000 - 백엔드 8446포트에서 테스트 할 때 사용하세요.
 // const GOOGLE_OAUTH_URL =
@@ -9,7 +10,9 @@ const FILE_PATH = BASE_URL + '/presentation';
 // const GOOGLE_OAUTH_URL =
 //   'https://localhost:8446/oauth2/authorize/google?redirect_uri=https://localhost:8443/front/oauth2/redirect';
 //배포
+// const GOOGLE_OAUTH_URL =
+//   'https://k5a204.p.ssafy.io/oauth2/authorize/google?redirect_uri=https://k5a204.p.ssafy.io/front/oauth2/redirect';
 const GOOGLE_OAUTH_URL =
-  'https://k5a204.p.ssafy.io/oauth2/authorize/google?redirect_uri=https://k5a204.p.ssafy.io/front/oauth2/redirect';
+  'https://northernplanet.p.ssafy.io/oauth2/authorize/google?redirect_uri=https://northernplanet.p.ssafy.io/front/oauth2/redirect';
 
 export { BASE_URL, GOOGLE_OAUTH_URL, FILE_PATH };
