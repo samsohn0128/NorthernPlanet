4e07c71d94838cea9c646da33409f48c1a48c0dd
손동우 <ssafy_coach_67@ssafy.com>
2022-04-05 17:31:18 +0900

    feat: add import.sql

---
+++ backend/northernplanet/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO `mysql-northernplanet`.code (`code_id`, `code_name`) VALUES ('001', '방장');
+INSERT INTO `mysql-northernplanet`.code (`code_id`, `code_name`) VALUES ('010', '평가자');
+INSERT INTO `mysql-northernplanet`.code (`code_id`, `code_name`) VALUES ('100', '발표자');
